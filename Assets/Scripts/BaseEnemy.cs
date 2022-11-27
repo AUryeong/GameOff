12ea435cf070b59f88772a5a1aafdfb9be4cdeb5
Black_Penguin <dkstmdwnswns@gmail.com>
2022-11-27 15:28:46 +0900

    UI Import

---
+++ Assets/Scripts/BaseEnemy.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using System.ComponentModel;
+
+public class BaseEnemy : MonoBehaviour, IInteractiveObj
+{
+    private SpriteRenderer spriteRenderer;
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    public void Interaction()
+    {
+        Killed();
+    }
+    private void Killed()
+    {
+        Instantiate(GameManager.Instance.enemyKilledParticle, transform.position, Quaternion.identity);
+        spriteRenderer.DOFade(0, 1).OnComplete(() => Destroy(gameObject));
+
+    }
+}
